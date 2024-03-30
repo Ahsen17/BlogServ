4347647cb3ccdeb707d95347c78b189d5ebf0dbc
Ahsen <54966541+Ahsen17@users.noreply.github.com>
2024-03-31 00:54:23 +0800

    Update cache.go

--- src/data/cache.go
+++ src/data/cache.go
@@ -9,22 +9,22 @@ import (
 //cache 选择redis
 
 var (
-	client *redis.Client
+	cache *redis.Client
 )
 
-func InitClient(c *config.Cache) *redis.Client {
-	if client != nil {
-		return client
+func InitCache(c *config.Cache) *redis.Client {
+	if cache != nil {
+		return cache
 	}
 
 	lock.Lock()
 	defer lock.Unlock()
 
-	client = redis.NewClient(&redis.Options{
+	cache = redis.NewClient(&redis.Options{
 		Addr:     fmt.Sprintf("%s:%d", c.Host, c.Port),
 		Password: c.Password,
 		DB:       c.Database,
 	})
 
-	return client
+	return cache
 }
