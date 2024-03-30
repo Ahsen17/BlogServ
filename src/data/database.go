4805d2c5276b9127f7bb5c5b9e4f239988df5320
Ahsen <54966541+Ahsen17@users.noreply.github.com>
2024-03-31 00:55:18 +0800

    Update database.go
    使用gorm内置的数据库连接池

--- src/data/database.go
+++ src/data/database.go
@@ -10,91 +10,75 @@ import (
 	"sync"
 )
 
-//database 选择mysql
-
-// InitDB 初始化数据库连接
-//func InitDB(c *config.Config) *gorm.DB {
-//	dbConf := c.Database
-//	dsnRaw := "%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local"
-//	// 若数据库不存在则创建()
-//	//dsn := fmt.Sprintf(dsnRaw, dbConf.Username, dbConf.Password, dbConf.Host, dbConf.Port, "")
-//	//
-//	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-//	//if err != nil {
-//	//	logger.Fatal("连接数据库失败")
-//	//}
-//	//
-//	//db.Exec("CREATE DATABASE IF NOT EXISTS " + dbConf.Database)
-//	dsn := fmt.Sprintf(dsnRaw, dbConf.Username, dbConf.Password, dbConf.Host, dbConf.Port, dbConf.Database)
-//	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-//	if err != nil {
-//		logger.Fatal("连接数据库失败")
-//	}
-//
-//	err = db.AutoMigrate(&models.User{})
-//	if err != nil {
-//		logger.Fatal("数据库表迁移失败")
-//	}
-//
-//	return db
-//}
-
 var (
-	dbPool *sql.DB
+	client *gorm.DB
 	lock   sync.Mutex
 )
 
-// InitDBPool 初始化数据库连接池
-func InitDBPool(db *config.Database) *sql.DB {
-	if dbPool != nil {
-		return dbPool
+const (
+	MYSQL  = "mysql"
+	SQLITE = "sqlite"
+)
+
+// InitDBClient 初始化数据库连接池
+func InitDBClient(dbConf *config.Database) *gorm.DB {
+	if client != nil {
+		return client
 	}
 
 	lock.Lock()
 	defer lock.Unlock()
 
-	driverName := db.Driver
+	driverName := dbConf.Driver
 	dsn := "%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local"
-	dataSourceName := fmt.Sprintf(dsn, db.Username, db.Password, db.Host, db.Port, db.Database)
+	dataSourceName := fmt.Sprintf(dsn, dbConf.Username, dbConf.Password,
+		dbConf.Host, dbConf.Port, dbConf.Database)
 
-	var err error
-	if dbPool, err = sql.Open(driverName, dataSourceName); err != nil {
-		logger.Errorf("创建数据库连接池失败: %s", err)
+	// 切换数据库驱动
+	var conn gorm.Dialector
+	switch driverName {
+	case MYSQL:
+		conn = mysql.Open(dataSourceName)
+	}
+
+	if conn != nil {
+		logger.Fatal("数据库连接校验失败")
+	}
+
+	var connPool *sql.DB
+	if client, err := gorm.Open(conn, &gorm.Config{}); err != nil {
+		logger.Errorf("连接数据库失败: %s", err)
 		panic(err)
+	} else {
+		connPool, err = client.DB()
+		if err != nil {
+			logger.Errorf("获取数据库连接池失败: %s", err)
+			panic(err)
+		}
 	}
 
 	// 设置最大连接数与最大空闲数
-	dbPool.SetMaxOpenConns(db.MaxConn)
-	dbPool.SetMaxIdleConns(db.IdleConn)
-
-	//defer func(dbPool *sql.DB) {
-	//	err := dbPool.Close()
-	//	if err != nil {
-	//		logger.Fatalf("关闭数据库连接池失败: %s", err)
-	//	}
-	//}(dbPool)
+	connPool.SetMaxOpenConns(dbConf.MaxConn)
+	connPool.SetMaxIdleConns(dbConf.IdleConn)
 
 	logger.Info("数据库连接池初始化完成")
-	return dbPool
+	return client
 }
 
 // Migrate 迁移数据库表
-func Migrate(sqlDB *sql.DB, objs ...any) error {
-	gormDB, err := gorm.Open(mysql.New(mysql.Config{Conn: sqlDB}), &gorm.Config{})
-	if err != nil {
-		logger.Errorf("从连接池获取数据库Conn失败: %s", err)
-		return err
-	}
-
+func Migrate(objs ...interface{}) error {
 	// 迭代迁移数据库表
-	for _, obj := range objs {
-		err := gormDB.AutoMigrate(&obj)
-		if err != nil {
-			logger.Errorf("迁移数据库表失败: %s", err)
-			return err
-		}
+	if err := client.AutoMigrate(objs); err != nil {
+		logger.Errorf("数据库表迁移失败")
+		return err
 	}
 
 	logger.Info("数据库表迁移完成")
 	return nil
+}
+
+func GetData(clue interface{}) (string, error) {
+	var err error
+
+	return "", err
 }
