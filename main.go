8aa76f8b3f857f91bda4f60d6f7824240b7335d7
Ahsen17 <ahsenedward@qq.com>
2024-04-20 21:41:51 +0800

    # 清空main分支代码,测试完成后从test分支merge

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"BlogServ/logger"
-	"fmt"
-)
-
 func main() {
-	logger.Info("test")
 
-	fmt.Println("hello golang")
-	//r := gin.Default()
-	//r.GET("/ping", func(c *gin.Context) {
-	//	c.JSON(http.StatusOK, gin.H{
-	//		"message": "pong",
-	//	})
-	//})
-	//
-	//r.Run(":9091")
 }
