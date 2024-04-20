1c84052ddd79700c5d04f764b1ef714dbdc40a2b
Ahsen17 <ahsenedward@qq.com>
2024-04-21 06:02:14 +0800

    # 通用的json响应

--- common/response.go
+++ common/response.go
@@ -12,39 +12,43 @@ const (
 )
 
 type Response struct {
-	Ctx     *gin.Context
 	Code    int         `json:"code"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
 }
 
-func (r Response) BASE(code int, msg string, data interface{}) {
-	r.Code = code
-	r.Message = msg
-	r.Data = data
-	r.Ctx.JSON(code, r)
+type ResponseMgr struct {
+	Ctx      *gin.Context
+	Response Response
+}
+
+func (mgr ResponseMgr) BASE(code int, msg string, data interface{}) {
+	mgr.Response.Code = code
+	mgr.Response.Message = msg
+	mgr.Response.Data = data
+	mgr.Ctx.JSON(code, &mgr.Response)
 }
 
 // OK 访问服务成功
-func (r Response) OK(msg string, data interface{}) {
+func (mgr ResponseMgr) OK(msg string, data interface{}) {
 	if msg == "" {
 		msg = SuccessAccess
 	}
-	r.BASE(http.StatusOK, msg, data)
+	mgr.BASE(http.StatusOK, msg, data)
 }
 
 // FAIL 访问服务失败
-func (r Response) FAIL(msg string, data interface{}) {
+func (mgr ResponseMgr) FAIL(msg string, data interface{}) {
 	if msg == "" {
 		msg = FailedAccess
 	}
-	r.BASE(http.StatusBadRequest, msg, data)
+	mgr.BASE(http.StatusBadRequest, msg, data)
 }
 
 // ERROR 应用程序内部异常
-func (r Response) ERROR(msg string, data interface{}) {
+func (mgr ResponseMgr) ERROR(msg string, data interface{}) {
 	if msg == "" {
 		msg = ServerError
 	}
-	r.BASE(http.StatusInternalServerError, msg, data)
+	mgr.BASE(http.StatusInternalServerError, msg, data)
 }
