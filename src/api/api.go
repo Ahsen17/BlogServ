98f4125e2f7b78f2b4ae4f5240e73a6290f11eb6
Ahsen17 <ahsenedward@qq.com>
2024-04-27 22:44:49 +0800

    # 调整路由

--- src/api/api.go
+++ src/api/api.go
@@ -1,3 +1,11 @@
+/*
+  Package api
+  @Author: Ahsen17
+  @Github: https://github.com/Ahsen17
+  @Time:
+  @Description: 平台接口管理
+*/
+
 package api
 
 import (
@@ -8,6 +16,33 @@ import (
 
 func init() {
 
+}
+
+func NoContentHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"code":    http.StatusOK,
+		"success": true,
+		"message": "无内容",
+		"data":    nil,
+	})
+}
+
+func NotFoundHandler(c *gin.Context) {
+	c.JSON(http.StatusNotFound, gin.H{
+		"code":    http.StatusNotFound,
+		"success": false,
+		"message": "未找到相关内容",
+		"data":    nil,
+	})
+}
+
+func ServerErrorHandler(c *gin.Context) {
+	c.JSON(http.StatusInternalServerError, gin.H{
+		"code":    http.StatusInternalServerError,
+		"success": false,
+		"message": "系统错误",
+		"data":    nil,
+	})
 }
 
 // BasePath在同一个模块中定义一次即可
--- src/router/router.go
+++ src/router/router.go
@@ -9,14 +9,12 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/ahsen17/BlogServ/src/api"
 	"github.com/gin-gonic/gin"
-
-	swaggerDocs "github.com/ahsen17/BlogServ/docs"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+
+	swaggerDocs "github.com/ahsen17/BlogServ/docs"
 )
 
 // 参考下开源项目RouterGroup的用法
@@ -26,52 +24,52 @@ func InitRouter(engine *gin.Engine) {
 	// 系统路由
 	mainGrp := engine.Group("")
 	{
-		mainGrp.GET("/", MainHandler)
-		mainGrp.GET("/404", NotFoundHandler)
-		mainGrp.GET("/500", ServerErrorHandler)
+		mainGrp.GET("/", api.NoContentHandler)
+		mainGrp.GET("/404", api.NotFoundHandler)
+		mainGrp.GET("/500", api.ServerErrorHandler)
 	}
 
+	// 数据路由
+	dataGrp := engine.Group("/data")
+	{
+		dataGrp.GET("", api.NoContentHandler)
+
+		dataGrp.Group("/account")
+
+		dataGrp.Group("/log")
+
+		dataGrp.Group("/module")
+
+		dataGrp.Group("/role")
+
+		userGrp := dataGrp.Group("/user")
+		userGrp.GET("/detail", api.NoContentHandler)
+	}
+
+	// 模块路由
+	moduleGrp := engine.Group("/module")
+	{
+		moduleGrp.GET("", api.NoContentHandler)
+	}
+
+	// 搜索路由
+	searchGrp := engine.Group("/search")
+	{
+		searchGrp.GET("", api.NoContentHandler)
+	}
+
+	// 通信路由
+	teleportGrp := engine.Group("/teleport")
+	{
+		teleportGrp.GET("", api.NoContentHandler)
+	}
+
+	// 接口文档: /api/docs/index.html
+	engine.GET("/api/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	// 应用接口
 	apiV1Grp := "/api/v1"
 	swaggerDocs.SwaggerInfo.BasePath = apiV1Grp
-	// 接口version 1
+	// 接口version 1st.
 	v1Grp := engine.Group(apiV1Grp)
-	{
-		v1Grp.GET("/example", api.SwaggerExample)
-	}
-
-	// 其他路由
-	{
-		// swagger接口文档
-		engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	}
-}
-
-// MainHandler 主页
-func MainHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"code":    http.StatusOK,
-		"success": true,
-		"message": "无内容",
-		"data":    nil,
-	})
-}
-
-// NotFoundHandler 404处理
-func NotFoundHandler(c *gin.Context) {
-	c.JSON(http.StatusNotFound, gin.H{
-		"code":    http.StatusNotFound,
-		"success": false,
-		"message": "未找到相关内容",
-		"data":    nil,
-	})
-}
-
-func ServerErrorHandler(c *gin.Context) {
-	c.JSON(http.StatusInternalServerError, gin.H{
-		"code":    http.StatusInternalServerError,
-		"success": false,
-		"message": "系统错误",
-		"data":    nil,
-	})
+	v1Grp.GET("/example", api.SwaggerExample)
 }
